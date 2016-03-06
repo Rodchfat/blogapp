e9804615a727c0bc41b287a073cbf3b7a00001c7
Rails Projects <rod-rails@gmail.com>
2016-03-06 21:32:22 +0000

    working on article controller

--- bloggy/app/controllers/articles_controller.rb
+++ bloggy/app/controllers/articles_controller.rb
@@ -7,21 +7,35 @@ class ArticlesController < ApplicationController
   end
 
   def edit
+    @article = Article.find(params[:id])
   end
 
   def create
     @article = Article.new(article_params)
-    @article.save
+    if @article.save 
+      flash[:notice] = "Article was successfully created"
+    redirect_to article_path(@article)
+    else
+      render 'new'
+    end
   end
-
   def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article Was Succesffuly updated"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
   end
 
   def show
+    @article = Article.find(params[:id])
   end
   
   private 
   def article_params
     params.require(:article).permit(:title, :description)
   end
+
 end
--- bloggy/app/models/article.rb
+++ bloggy/app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ActiveRecord::Base
+    validates :title, presence: true, length: {minimun:10, maximum: 30}
+    validates :description, presence: true, length: {minimum:10,maximum: 30}
     
     
 end
